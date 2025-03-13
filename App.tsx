07dc492412b27f490603075754ddf0344fb5f1d3
Assisneto Damasceno <netodamasceno12@gmail.com>
2025-03-13 19:02:20 -0300

    add Prettier configuration and format code for consistency

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import {  Text,  } from 'react-native';
-import styled from 'styled-components/native';
+import { StatusBar } from "expo-status-bar";
+import { Text } from "react-native";
+import styled from "styled-components/native";
 
 const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
-
   background-color: #224123;
 `;
 
