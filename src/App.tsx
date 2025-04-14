65b460a672cb38d2fb118559fecfbf3b6cd3277c
Assisneto Damasceno <netodamasceno12@gmail.com>
2025-04-13 22:16:05 -0300

    feat: implement navigation stack with Home and TinderCard screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
-import Home from "./screens/home";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import TinderCard from "./screens/tinderCard";
+import Home from "./screens/Home";
+
+type RootStackParamList = {
+  Home: undefined;
+  TinderCard: { disciplineId: string };
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
-  return <Home />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="TinderCard"
+          component={TinderCard}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
