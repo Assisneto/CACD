76e111a62e13cd4beda5ac511458bc3cfdbe175a
Assisneto Damasceno <netodamasceno12@gmail.com>
2025-05-12 19:47:58 -0300

    feat: update Firebase configuration to use environment variables

--- src/config/firebase/firebase.ts
+++ src/config/firebase/firebase.ts
@@ -1,19 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCPS1nIVnhcPlnLZxwMmu3NHcaM73z4NyQ",
-  authDomain: "qzly-36138.firebaseapp.com",
-  projectId: "qzly-36138",
-  storageBucket: "qzly-36138.firebasestorage.app",
-  messagingSenderId: "760517676150",
-  appId: "1:760517676150:web:5b84f6946cfa8024d7e357",
-  measurementId: "G-W2Y3ZDDGB5"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
